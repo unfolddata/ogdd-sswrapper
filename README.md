630b8abb605ea5c25e2a5028b5a779814a99c718
Timo Franz <timo@dumpark.com>
2018-07-05 15:35:30 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# OGDD Silverstripe Wrapper Module
---
+++ src/OGDDPage.php
@@ -0,0 +1,10 @@
+<?php
+
+use SilverStripe\CMS\Model\SiteTree;
+
+class OGDDPage extends SiteTree
+{
+    private static $db = [];
+
+    private static $has_one = [];
+}
